e87efc6a7c52cb5103b2c6d1251221d50ac0e73b
Daniel-Constantin Mierla <miconda@gmail.com>
2020-11-21 08:43:34 +0100

    added u cmd to expoand to ksr-url-expander github project page

--- background.js
+++ background.js
@@ -367,6 +367,8 @@ chrome.omnibox.onInputEntered.addListener(function(text) {
     fullURL = 'https://github.com/kamailio/kamailio/pulls';
   } else if (inText === "ke") {
     fullURL += '/docs/tutorials/devel/kamailio-kemi-framework/';
+  } else if (inText === "u") {
+    fullURL = 'https://github.com/miconda/KSR-URL-Expander';
   } else if (inText.indexOf("s ") === 0) {
     fullURL = 'https://www.google.com/search?q=' + inText.substr(2).trim() + ' site:kamailio.org';
   } else if (inText.indexOf("sl ") === 0) {
